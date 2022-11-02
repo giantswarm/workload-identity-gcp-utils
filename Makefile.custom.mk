b58cb4b25045f46eabdebc1953b2b87f0ef93353
George Gelaga-King <gb7309@gmail.com>
2022-11-02 17:25:00 +0200

    Add entries in `make help` and README instructions
    Co-authored-by: Mario Nitchev <mario@giantswarm.io>

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -11,32 +11,32 @@ ifndef GCP_PROJECT_ID
 endif
 
 .PHONY: all
-all: capg-firewall-rule-operator capg-operator cluster-control-plane-node cluster-worker-node csi-driver-app dns-operator-gcp external-dns-app fleet-membership-operator-gcp
+all: capg-firewall-rule-operator cluster-api-provider-gcp-app cluster-control-plane-node cluster-worker-node csi-driver-app all external-dns-app fleet-membership-operator-gcp ## Install all service accounts. See `make help` for a list of all the available components.
 
 .PHONY: capg-firewall-operator
-capg-firewall-rule-operator: ensure-gcloud-exists ensure-gcp-envs
+capg-firewall-rule-operator: ensure-gcloud-exists ensure-gcp-envs ## Installs the service account for capg-firewall-rule-operator
 	SA_DISPLAY_NAME=capg-firewall-rule-operator \
 	KUBE_NAMESPACE=giantswarm \
 	KUBE_SERVICE_ACCOUNT=capg-firewall-rule-operator \
 	WORKLOAD_IDENTITY_ENABLED=true \
 	./scripts/configure-service-account.sh
 
-.PHONY: capg-operator
-capg-operator: ensure-gcloud-exists ensure-gcp-envs
+.PHONY: cluster-api-provider-gcp-app
+cluster-api-provider-gcp-app: ensure-gcloud-exists ensure-gcp-envs ## Installs the service account for cluster-api-provider-gcp-app
 	./scripts/configure-capg-operator.sh
 
 .PHONY: cluster-control-plane-node
-cluster-control-plane-node: ensure-gcloud-exists ensure-gcp-envs
+cluster-control-plane-node: ensure-gcloud-exists ensure-gcp-envs ## Installs the service account for cluster-control-plane-node
 	SA_DISPLAY_NAME=cluster-control-plane-node \
 	./scripts/configure-service-account.sh
 
 .PHONY: cluster-worker-node
-cluster-worker-node: ensure-gcloud-exists ensure-gcp-envs
+cluster-worker-node: ensure-gcloud-exists ensure-gcp-envs ## Installs the service account for cluster-worker-node
 	SA_DISPLAY_NAME=cluster-worker-node \
 	./scripts/configure-service-account.sh
 
 .PHONY: csi-driver-app
-csi-driver-app: ensure-gcloud-exists ensure-gcp-envs
+csi-driver-app: ensure-gcloud-exists ensure-gcp-envs ## Installs the service account for csi-driver-app
 	SA_DISPLAY_NAME=gcp-compute-persistent-disk-csi-driver-app \
 	KUBE_NAMESPACE=kube-system \
 	KUBE_SERVICE_ACCOUNT=csi-gce-pd-controller-sa \
@@ -44,7 +44,7 @@ csi-driver-app: ensure-gcloud-exists ensure-gcp-envs
 	./scripts/configure-service-account.sh
 
 .PHONY: dns-operator-gcp
-dns-operator-gcp: ensure-gcloud-exists ensure-gcp-envs
+dns-operator-gcp: ensure-gcloud-exists ensure-gcp-envs ## Installs the service account for dns-operator-gcp
 	SA_DISPLAY_NAME=dns-operator-gcp \
 	KUBE_NAMESPACE=giantswarm \
 	KUBE_SERVICE_ACCOUNT=dns-operator-gcp \
@@ -52,7 +52,7 @@ dns-operator-gcp: ensure-gcloud-exists ensure-gcp-envs
 	./scripts/configure-service-account.sh
 
 .PHONY: external-dns-app
-external-dns-app: ensure-gcloud-exists ensure-gcp-envs
+external-dns-app: ensure-gcloud-exists ensure-gcp-envs ## Installs the service account for external-dns-app
 	SA_DISPLAY_NAME=external-dns-app \
 	KUBE_NAMESPACE=kube-system \
 	KUBE_SERVICE_ACCOUNT=external-dns-app \
@@ -60,6 +60,6 @@ external-dns-app: ensure-gcloud-exists ensure-gcp-envs
 	./scripts/configure-service-account.sh
 
 .PHONY: fleet-membership-operator-gcp
-fleet-membership-operator-gcp: ensure-gcloud-exists ensure-gcp-envs
+fleet-membership-operator-gcp: ensure-gcloud-exists ensure-gcp-envs ## Installs the service account for fleet-membership-operator-gcp
 	SA_DISPLAY_NAME=fleet-membership-operator-gcp \
 	./scripts/configure-service-account.sh
--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 
 A collection of scripts to help you configure and use workload identity on GCP.
 
+## Usage
+
+### Prerequisites 
+
+1. Install [gcloud cli](https://cloud.google.com/sdk/docs/install)
+1. Run `gcloud auth login`
+1. Make sure you log in with a user that has the permissions to create service
+   accounts, roles and assing roles to service accounts on the project level as
+   well as assign service account users.
+
+### Installing everyting
+
+To install all service accounts simply run:
+```sh
+GCP_PROJECT=<your-project-id> make all
+```
+
+_NOTE:_ You can also just export the `GCP_PROJECT` env var instead of passing it to each make call.
+
+### Install specific service account
+
+You can install a specific service account by calling it's make target. To get a list of all available targets run `make help`
+```sh
+GCP_PROJECT=<your-project-id> make <some-service-account-target>
+```
